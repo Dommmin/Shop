e244bdc2ca2cb9fef8951bca0c026fe2cad28d69
Dominik <d.jasinski@ideo.pl>
2022-10-24 21:38:58 +0200

    init commit

---
+++ resources/js/Pages/Admin/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <AdminLayout title="Index">
+        <template #header>
+            Index
+        </template>
+
+        Admin...
+
+    </AdminLayout>
+</template>
+
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+
+</script>
